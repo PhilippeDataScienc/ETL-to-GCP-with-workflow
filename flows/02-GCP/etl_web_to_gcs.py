f6b4684a8eb32ba436f9b4e76741f4596102595b
Philippe <philippe.acquier@gmail.com>
2023-10-11 10:49:11 +0200

    save

--- flows/02-GCP/etl_web_to_gcs.py
+++ flows/02-GCP/etl_web_to_gcs.py
@@ -2,6 +2,26 @@ from pathlib import Path
 import pandas as pd
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
+from prefect.tasks import task_input_hash
+from datetime import timedelta
+
+
+@task(retries=3,  cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
+def fetch(dataset_url: str) -> pd.DataFrame:
+    """Read data from web into pandas DataFrame"""
+
+    df = pd.read_csv(dataset_url)
+    return df
+
+@task(log_prints=True)
+def clean(df: pd.DataFrame) -> pd.DataFrame:
+    """Fix some dtypes issues"""
+    df['tpep_pickup_datetime']=pd.to_datetime(df['tpep_pickup_datetime'])
+    df['tpep_dropoff_datetime']=pd.to_datetime(df['tpep_dropoff_datetime'])
+    print(df.head(2))
+    print(f'columns : {df.dtypes}')
+    print(f"rows : {len(df)}")
+    return df
 
 @flow()
 def etl_web_to_gcs() -> None:
@@ -12,3 +32,9 @@ def etl_web_to_gcs() -> None:
     month = 1
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
+
+    df = fetch(dataset_url)
+    df_clean = clean(df)
+
+if __name__=="__main__":
+    etl_web_to_gcs()
